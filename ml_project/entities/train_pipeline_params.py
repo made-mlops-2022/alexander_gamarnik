9533fef42e1db07d66a7ee1ac4539480dbdc5e3e
alexwerben <crystall.werben@gmail.com>
2022-11-05 15:05:16 +0300

    unit tests added

--- ml_project/entities/train_pipeline_params.py
+++ ml_project/entities/train_pipeline_params.py
@@ -6,7 +6,10 @@ from .download_params import DownloadParams
 from .split_params import SplittingParams
 from .feature_params import FeatureParams
 from .train_params import TrainingParams
+from .model_params import RFParams, LogregParams
+from .transform_params import TransformParams
 from marshmallow_dataclass import class_schema
+from hydra.core.config_store import ConfigStore
 import yaml
 
 
@@ -22,6 +25,7 @@ class TrainingPipelineParams:
     splitting_params: SplittingParams
     feature_params: FeatureParams
     train_params: TrainingParams
+    transform_params: Optional[TransformParams] = None
     downloading_params: Optional[DownloadParams] = None
     use_mlflow: bool = False
     mlflow_uri: str = "http://18.156.5.226/"
@@ -35,3 +39,17 @@ def read_training_pipeline_params(path: str) -> TrainingPipelineParams:
     with open(path, "r") as input_stream:
         schema = TrainingPipelineParamsSchema()
         return schema.load(yaml.safe_load(input_stream))
+    
+def register_train_configs() -> None:
+    cs = ConfigStore.instance()
+    cs.store(name="config", node=TrainingPipelineParams)
+    cs.store(
+        group="model",
+        name="rf",
+        node=RFParams
+    )
+    cs.store(
+        group="model",
+        name="logreg",
+        node=LogregParams
+    )
--- tests/test_units.py
+++ tests/test_units.py
@@ -6,14 +6,16 @@ from nbformat import read
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-
+from sklearn.pipeline import Pipeline
 
 import pytest
 
 
 from hydra import initialize, compose
-# from hydra.utils import instantiate
+from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
+
+from ml_project.entities.train_params import TrainingParams
 
 sys.path.append("../")
 sys.path.append(os.path.join(os.path.abspath(".")))
@@ -29,16 +31,8 @@ from ml_project.features.build_features import (
     build_categorical_pipeline,
     process_numerical_features,
     build_numerical_pipeline,
-    make_features,
     build_transformer,
     extract_target                                 
-)
-from ml_project.models.model_fit_predict import (
-    train_model,
-    serialize_model,
-    predict_model,
-    evaluate_model,
-    create_inference_pipeline,
 )
 
 
@@ -49,7 +43,8 @@ CONFIG_FILES = [f.split(".")[0] for f in os.listdir("configs") if "yaml" in f]
 def test_read_data(path="./data/processed/heart_cleveland_upload.csv"):
     data = read_data(path)
     assert isinstance(data, pd.DataFrame)
-    
+
+
 def test_split_train_val_data(synthetic_train_data, train_config):
     train_df, val_df = split_train_val_data(
         synthetic_train_data, train_config.splitting_params
@@ -58,11 +53,45 @@ def test_split_train_val_data(synthetic_train_data, train_config):
         train_config.splitting_params.val_size, 0.01
     )
 
+
+def test_process_categorical_features(synthetic_train_data):
+    df = process_categorical_features(synthetic_train_data)
+    assert isinstance(df, pd.DataFrame)
+
+
+def test_build_categorical_pipeline():
+    pipeline = build_categorical_pipeline()
+    assert isinstance(pipeline, Pipeline)
+
+
+def test_process_numerical_features(synthetic_train_data):
+    df = process_numerical_features(synthetic_train_data)
+    assert isinstance(df, pd.DataFrame)
+
+
+def test_build_numerical_pipeline():
+    pipeline = build_numerical_pipeline()
+    assert isinstance(pipeline, Pipeline)
+
+
+def test_build_transformer(train_config):
+    transformer = build_transformer(
+        train_config.feature_params
+    )
+    assert isinstance(transformer, ColumnTransformer)
+
+
+def test_extract_target(synthetic_train_data, train_config):
+    train_target = extract_target(
+        synthetic_train_data, train_config.feature_params
+    )
+    assert set(train_target.values) == {0, 1}
+
+
 @pytest.fixture()
 def train_config():
-    # ml_project.entities.register_train_configs()
-    # ml_project.entities.read_training_pipeline_params("./configs/train_config.yaml")
-    with initialize(config_path="./configs"):
+    ml_project.entities.train_pipeline_params.register_train_configs()
+    with initialize(version_base=None, config_path="../configs"):
         train_params = compose(config_name="train_config")
     return train_params
 
